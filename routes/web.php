08bfb21da56242ea48c671fe85b77bea12653610
Hung Do <hung96bk@gmail.com>
2018-07-20 14:13:30 +0700

    phân quyền

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,42 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('admins', 'AdminController');
-Route::resource('categories', 'CategoryController');
-Route::resource('certificates', 'CertificateController');
-Route::resource('comments', 'CommentController');
-Route::resource('deals', 'DealController');
-Route::resource('favorites', 'FavoriteController');
-Route::resource('foods', 'FoodController');
-Route::resource('food_images', 'FoodImageController');
-Route::resource('category_images', 'CategoryImageController');
-Route::resource('transactions', 'TransactionController');
-Route::resource('users', 'UserController');
-Route::resource('votes', 'VoteController');
+Route::middleware(['auth'])->group(function () {
+    $index_show = ['index', 'show'];
+    Route::resource('admins', 'AdminController');
+    Route::resource('categories', 'CategoryController')->except($index_show);
+    Route::resource('certificates', 'CertificateController')->except(
+        $index_show
+    );
+    Route::resource('comments', 'CommentController')->except($index_show);
+    Route::resource('deals', 'DealController')->except($index_show);
+    Route::resource('favorites', 'FavoriteController')->except($index_show);
+    Route::resource('foods', 'FoodController')->except($index_show);
+    Route::resource('food_images', 'FoodImageController')->except($index_show);
+    Route::resource('category_images', 'CategoryImageController')->except(
+        $index_show
+    );
+    Route::resource('transactions', 'TransactionController')->except(
+        $index_show
+    );
+    Route::resource('users', 'UserController')->except($index_show);
+    Route::resource('votes', 'VoteController')->except($index_show);
+});
+
+$index_show = ['index', 'show'];
+Route::resource('categories', 'CategoryController')->only($index_show);
+Route::resource('certificates', 'CertificateController')->only($index_show);
+Route::resource('comments', 'CommentController')->only($index_show);
+Route::resource('deals', 'DealController')->only($index_show);
+Route::resource('favorites', 'FavoriteController')->only($index_show);
+Route::resource('foods', 'FoodController')->only($index_show);
+Route::resource('food_images', 'FoodImageController')->only($index_show);
+Route::resource('category_images', 'CategoryImageController')->only(
+    $index_show
+);
+Route::resource('transactions', 'TransactionController')->only($index_show);
+Route::resource('users', 'UserController')->only($index_show);
+Route::resource('votes', 'VoteController')->only($index_show);
 
 Route::get('test', 'UserController@getUser');
 
